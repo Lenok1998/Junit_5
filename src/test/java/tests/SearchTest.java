c5ab2a3d2b9317135776c3c6e26e0098f72ccda9
Elena <39148920+Lenok1998@users.noreply.github.com>
2023-11-21 23:45:50 +0400

    Fix SearchTest.java

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -2,7 +2,6 @@ package tests;
 
 import data.Language;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
